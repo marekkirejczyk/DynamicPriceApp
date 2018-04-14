3950c6db196f8a895ab804b0b741a872de6e5d19
Marek Kirejczyk <marek.kirejczyk@gmail.com>
2018-04-14 13:41:37 +0200

    refactor

--- public/app.js
+++ public/app.js
@@ -1,18 +1,34 @@
+class Application {
 
-outputContainer = document.getElementById("output");
-outputMessage = document.getElementById("outputMessage");
-outputData = document.getElementById("outputData");
+  start() {
+    // console.log(document.getElementById("makeitbigger"));
+    document.getElementById("makeitbigger").onclick = this.navigateToQrCode;
+  }
 
-function onQRCodeScanCallback(code) {
-  outputMessage.hidden = true;
-  outputData.parentElement.hidden = false;
-  outputData.innerText = code.data;
+  navigateToQrCode() {
+    window.location.href = './qrscan.html';
+  }
+
+  // setupScanQR() {
+  //   outputContainer = document.getElementById("output");
+  //   outputMessage = document.getElementById("outputMessage");
+  //   outputData = document.getElementById("outputData");
+    
+  //   function onQRCodeScanCallback(code) {
+  //     outputMessage.hidden = true;
+  //     outputData.parentElement.hidden = false;
+  //     outputData.innerText = code.data;
+  //   }
+    
+  //   function onNoQRCodeScanCallback() {
+  //     outputMessage.hidden = false;
+  //     outputData.parentElement.hidden = true;
+  //   }
+    
+  //   new QRCodeReader(onQRCodeScanCallback, onNoQRCodeScanCallback)
+  // }
+
 }
 
-function onNoQRCodeScanCallback() {
-  outputMessage.hidden = false;
-  outputData.parentElement.hidden = true;
-}
+new Application().start();
 
-
-new QRCodeReader(onQRCodeScanCallback, onNoQRCodeScanCallback)
